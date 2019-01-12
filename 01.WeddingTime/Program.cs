4c31e15d34a96b3c99303d469555ca04ee2b4be7
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-12 22:04:58 +0200

    01.WeddubgTime

---
+++ 01.WeddingTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01.WeddingTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceWhiskey = double.Parse(Console.ReadLine());
+            double quantityWater = double.Parse(Console.ReadLine());
+            double quantityWine = double.Parse(Console.ReadLine());
+            double quantityChampagne = double.Parse(Console.ReadLine());
+            double quantityWhiskey = double.Parse(Console.ReadLine());
+
+            double priceChampagne = priceWhiskey - priceWhiskey * 0.5;
+            double priceWine = priceChampagne - 0.6 * priceChampagne;
+            double priceWater = priceChampagne - 0.9 * priceChampagne;
+
+            double totalExpenses = priceWhiskey * quantityWhiskey + priceChampagne * quantityChampagne + priceWine * quantityWine + priceWater * quantityWater;
+            Console.WriteLine($"{totalExpenses:F2}");
+        }
+    }
+}
