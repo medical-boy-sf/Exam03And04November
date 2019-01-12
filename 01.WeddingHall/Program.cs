a1d0529377a19313242380b7ca62adf071b5bd1a
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-12 21:38:26 +0200

    init

---
+++ 01.WeddingHall/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01.WeddingHall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double sideBar = double.Parse(Console.ReadLine());
+            string message = String.Empty;
+
+            double totalArea = width * length;
+            double barArea = Math.Pow(sideBar, 2);
+            double dancingArea = 0.19 * totalArea;
+
+            double freeArea = totalArea - (barArea + dancingArea);
+
+            int numberOfPeople = (int)Math.Ceiling(freeArea / 3.2);
+
+            Console.WriteLine(numberOfPeople);
+        }
+    }
+}
