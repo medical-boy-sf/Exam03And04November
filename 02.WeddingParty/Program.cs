bf66ab4fef39962dd1b883567de107ce59cbb7d1
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-01-12 22:28:28 +0200

    02. Wedding Party

---
+++ 02.WeddingParty/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02.WeddingParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberGuests = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+
+            int expenses = numberGuests * 20;
+
+            if (budget >= expenses)
+            {
+                int remaining = budget - expenses;
+                double expensesFirework = 0.4 * remaining;
+                Console.WriteLine($"Yes! {(Math.Round(expensesFirework))} lv are for fireworks and {Math.Round(remaining - expensesFirework)} lv are for donation.");
+            }
+
+            else
+            {
+                Console.WriteLine($"They won't have enough money to pay the covert. They will need {expenses - budget} lv more.");
+            }
+        }
+    }
+}
